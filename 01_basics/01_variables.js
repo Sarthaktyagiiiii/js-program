3988de1b0f14ef5451623576e1837540f54802b6
Sarthaktyagiiiii <117930415+Sarthaktyagiiiii@users.noreply.github.com>
2025-07-16 10:22:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const AccountID = 12345678
+let AccountEmail ="abcd@gml.com"
+var AccountPassword ="jsdbfjsnjvksnf"
+Account_city ="delhi"
+
+// const AccountID = 2314144   const cannot be change one declared
+AccountEmail="ghhgh@gml.com"  //let is used with is scope 
+AccountPassword ="fbsdbjsd"   //var allow to change all values inside code
+Account_city="noida"
+
+// console.table allow user to show data/info in  table format
+
+
+
+console.table([ AccountID,AccountEmail,AccountPassword,Account_city]);
---
+++ 01_basics/2_datatype.js
@@ -0,0 +1,7 @@
+// Number => 2 to power 53
+// BigInt=> use with big value
+// String=>""
+// Boolean=> true/false
+// null=> standalone value
+// undefined=>
+// symbol=>    
